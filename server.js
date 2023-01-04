31afcde91af7116f5645316fa7146dfefd2c73c6
Reginaldo Santos <reginaldo.santos@redspace.com>
2023-01-04 17:10:22 -0400

    feat: query all authors and its related books

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLNonNull(GraphQLInt) },
         name: { type: GraphQLNonNull(GraphQLString) },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (author) => {
+                return books.filter(book => book.authorId === author.id)
+            }
+        }
     })
 })
 
